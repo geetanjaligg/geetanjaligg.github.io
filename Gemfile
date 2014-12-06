a705ea27db4ba63cd484a2a2751d0edf521c01ea
geetanjaligg <anjaligg@gmail.com>
2014-12-06 16:51:31 +0530

    flick images and css changes

---
+++ Gemfile
@@ -0,0 +1,12 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+# gem "rails"
+gem 'jekyll', '~> 2.4.0'
+gem 'flickraw' 
+gem 'jekyll-assets', '~> 0.10.1'
+gem 'uglifier', '~> 2.5.0'
+# gem 'foundation-rails', '~> 5.2.2.0'
+gem 'i18n', '~> 0.6.11'
+# When I will think about related_post at the bottom of each post
+# gem 'rb-gsl', '~> 1.16.0.2'
---
+++ flickr.rb
@@ -0,0 +1,19 @@
+require 'flickraw'
+
+FlickRaw.api_key="dbb73d4857727852c56af56f7380d289"
+FlickRaw.shared_secret="2e9344cd6f01362e"
+
+token = flickr.get_request_token
+auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'delete')
+
+puts "Open this url in your process to complete the authication process : #{auth_url}"
+puts "Copy here the number given when you complete the process."
+verify = gets.strip
+
+begin
+  flickr.get_access_token(token['oauth_token'], token['oauth_token_secret'], verify)
+  login = flickr.test.login
+  puts "You are now authenticated as #{login.username} with token #{flickr.access_token} and secret #{flickr.access_secret}"
+rescue FlickRaw::FailedResponse => e
+  puts "Authentication failed : #{e.msg}"
+end
